fd0781ad12beaafa2f749f02e8228ce77c29ceae
mydearxym <mydearxym@qq.com>
2014-10-30 07:05:25 +0800

    layout degian and binding, keyboard return func

--- LoveFinder/LoveFinder/ViewController.swift
+++ LoveFinder/LoveFinder/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var gender: UISegmentedControl!
+    
+    @IBOutlet weak var birthday: UIDatePicker!
+    
+    @IBOutlet weak var heightNumber: UISlider!
+    
+    @IBOutlet weak var height: UILabel!
+    
+    @IBOutlet weak var hasProperty: UISwitch!
+    
+    @IBOutlet weak var result: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        name.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func heightChanged(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func okTapped(sender: AnyObject) {
 
+    }
+
+    // UITextFiledDelegate, 让键盘消失
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
 }
 
