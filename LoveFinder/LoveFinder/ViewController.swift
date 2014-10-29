993e7e7938cb4d41b61f9796ffa55e7b1103db70
mydearxym <mydearxym@qq.com>
2014-10-30 07:30:06 +0800

    basic done but buttom textfeild not show

--- LoveFinder/LoveFinder/ViewController.swift
+++ LoveFinder/LoveFinder/ViewController.swift
@@ -37,11 +37,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func heightChanged(sender: AnyObject) {
+        var slider = sender as UISlider
+        var i = Int(slider.value)
+        //slider.value = Float(i)
+
+        height.text = "\(i)cm"
         
     }
     
     @IBAction func okTapped(sender: AnyObject) {
+        let genderText = gender.selectedSegmentIndex == 0 ? "高富帅":"白富美"
+        let gregorian = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        
+        let now = NSDate()
+        let components = gregorian.components(NSCalendarUnit.YearCalendarUnit, fromDate: birthday.date, toDate: now, options: NSCalendarOptions(0))
 
+        let age = components.year
+        let hasPropertyText = hasProperty.on ? "有房":"无房"
+        
+        result.text = "\(name.text), \(age)岁, \(genderText), 身高:\(height.text), \(hasProperty), 求交往!"
+        println(result.text)
     }
 
     // UITextFiledDelegate, 让键盘消失
