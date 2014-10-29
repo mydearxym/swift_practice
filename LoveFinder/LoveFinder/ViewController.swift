b3f954621a2649a1a3467544f87b4c53cedcc5d0
mydearxym <mydearxym@qq.com>
2014-10-29 22:24:26 +0800

    LoverFinder app init

---
+++ LoveFinder/LoveFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveFinder
+//
+//  Created by mydear-xym on 14-10-29.
+//  Copyright (c) 2014年 xieyiming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
