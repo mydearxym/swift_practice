9be43ff076c5f4f0ec493192ad4af30011a0637c
mydearxym <mydearxym@qq.com>
2014-11-03 21:34:15 +0800

    cloud layout follow the book

---
+++ iOS8Weather/iOS8Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS8Weather
+//
+//  Created by mydear-xym on 14-11-3.
+//  Copyright (c) 2014年 xieyiming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
