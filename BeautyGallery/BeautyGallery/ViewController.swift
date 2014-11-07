fb03e3a153bc1842f9e3a7a4d473ff49f5907fdf
mydearxym <mydearxym@qq.com>
2014-11-07 20:47:00 +0800

    beauty Gallery change

---
+++ BeautyGallery/BeautyGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeautyGallery
+//
+//  Created by mydear-xym on 14-11-6.
+//  Copyright (c) 2014年 xieyiming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
